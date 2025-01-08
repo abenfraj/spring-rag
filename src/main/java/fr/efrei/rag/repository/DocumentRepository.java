e3e3369f6301bf683a5cd73265105544be4d044f
abenfraj <ayoub.benfraj013@gmail.com>
2025-01-08 17:01:10 +0100

    #4 Create document entity

---
+++ src/main/java/fr/efrei/rag/repository/DocumentRepository.java
@@ -0,0 +1,17 @@
+package fr.efrei.rag.repository;
+
+import fr.efrei.rag.domain.Document;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import fr.efrei.rag.repository.dto.DocumentDTO;
+
+import java.util.List;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    @Query(
+            "select distinct new fr.efrei.rag.repository.dto.DocumentDTO(d.id, d.title) from Document d"
+    )
+    List<DocumentDTO> findAllDocumentDTO();
+}
---
+++ src/main/java/fr/efrei/rag/web/rest/DocumentResource.java
@@ -0,0 +1,53 @@
+package fr.efrei.rag.web.rest;
+
+import fr.efrei.rag.domain.Document;
+import fr.efrei.rag.service.DocumentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class DocumentResource {
+    private final Logger log = LoggerFactory.getLogger(DocumentResource.class);
+
+    private final DocumentService documentService;
+
+    public DocumentResource(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostMapping("/document")
+    public ResponseEntity<Document> createDocument(@RequestBody Document document) throws URISyntaxException {
+        log.debug("REST request to save Document : {}", document);
+        Document result = documentService.buildAndSave(document);
+        return ResponseEntity
+                .created(new URI("/documents/" + result.getId()))
+                .body(result);
+    }
+
+    @GetMapping("/document")
+    public ResponseEntity<List<Document>> getAllDocuments() {
+        log.debug("REST request to get all Documents");
+        List<Document> documents = documentService.findAll();
+        return ResponseEntity.ok().body(documents);
+    }
+
+    @GetMapping("/document/{id}")
+    public ResponseEntity<Document> getDocument(@PathVariable Long id) {
+        log.debug("REST request to get Document : {}", id);
+        Document document = documentService.findById(id);
+        return ResponseEntity.ok().body(document);
+    }
+
+    @DeleteMapping("/document/{id}")
+    public ResponseEntity<Void> deleteDocument(@PathVariable Long id) {
+        log.debug("REST request to delete Document : {}", id);
+        documentService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
